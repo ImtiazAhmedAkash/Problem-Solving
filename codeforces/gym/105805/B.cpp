31c0a1a766c928f8d8e3f815be51051eb48a1280
ImtiazAhmedAkash <110800373+ImtiazAhmedAkash@users.noreply.github.com>
2025-04-22 11:34:17 +0600

    added codeforces #gym_105805: A, B, C

---
+++ codeforces/gym/105805/B.cpp
@@ -0,0 +1,46 @@
+/*  In the Name of ALLAH, the most gracious, the most merciful  */
+
+/*  And whoever puts their trust in ALLAH, He alone is sufficient for them. Al-Quran (65:3)  */
+
+// B - Kaosar Loves Divisors
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef LOCAL
+#include "Dbug.h"
+#else
+#define dbg(...)
+#endif
+
+using ll = long long;
+constexpr char nl = '\n';
+
+const int inf = 1e9;
+const ll INF = 1e18;
+const int mod = 998244353;
+const int N = 2e5 + 5;
+
+void solve() {
+    ll n;
+    cin >> n;
+    int cnt = 0;
+    while (n % 2 == 0) {
+        cnt++;
+        n /= 2;
+    }
+    cout << cnt / 2 << nl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    
+    int T = 1;
+    cin >> T;
+    for (int t = 1; t <= T; t++) {
+        solve();
+    }
+    
+    return 0;
+}
