ac59fc037fb4c9a84ba39ab1c1144ae9d6084f8f
ImtiazAhmedAkash <110800373+ImtiazAhmedAkash@users.noreply.github.com>
2025-04-22 11:36:41 +0600

    added codeforces #2095: A

---
+++ codeforces/2095/A.cpp
@@ -0,0 +1,38 @@
+/*  In the Name of ALLAH, the most gracious, the most merciful  */
+
+/*  And whoever puts their trust in ALLAH, He alone is sufficient for them. Al-Quran (65:3)  */
+
+// A - Piecing It Together
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef LOCAL
+#include "Dbug.h"
+#else
+#define dbg(...)
+#endif
+
+using ll = long long;
+constexpr char nl = '\n';
+
+const int inf = 2e9;
+const ll INF = 1e18;
+const int mod = 1e9 + 7;
+const int N = 1e7;
+
+void solve() {
+    cout << "puzzling" << nl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    
+    int T = 1;
+    for (int t = 1; t <= T; t++) {
+        solve();
+    }
+    
+    return 0;
+}
