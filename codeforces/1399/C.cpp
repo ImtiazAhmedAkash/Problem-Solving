5408139cd316035a0ab6f1ae6e95c3be3974349e
ImtiazAhmedAkash <110800373+ImtiazAhmedAkash@users.noreply.github.com>
2025-04-26 11:20:19 +0600

    added codeforces #1399: C

---
+++ codeforces/1399/C.cpp
@@ -0,0 +1,59 @@
+/*  In the Name of ALLAH, the most gracious, the most merciful  */
+
+/*  And whoever puts their trust in ALLAH, He alone is sufficient for them. Al-Quran (65:3)  */
+
+// C - Boats Competition
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef LOCAL
+#include "Dbug.h"
+#else
+#define dbg(...)
+#endif
+
+using ll = long long;
+constexpr char nl = '\n';
+
+const int inf = 1e9;
+const ll INF = 1e18;
+const int mod = 998244353;
+const int N = 2e5 + 5;
+
+void solve() {
+    int n;
+    cin >> n;
+    map<int, int> cnt;
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+        cnt[x]++;
+    }
+
+    int ans = 0;
+    for (int sum = 1; sum <= 2 * n; sum++) {
+        int cur = 0;
+        for (auto &[a, b] : cnt) {
+            cur += min(b, cnt[sum - a]);
+        }
+        cur /= 2;
+        ans = max(ans, cur);
+    }
+
+
+    cout << ans << nl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    
+    int T = 1;
+    cin >> T;
+    for (int t = 1; t <= T; t++) {
+        solve();
+    }
+    
+    return 0;
+}
